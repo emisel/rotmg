fe4a6701f37da685b2d81a4634c6b368a95a02b6
emisel <eselmeryd@gmail.com>
2014-08-24 22:00:21 +0200

    Ep 3 - Game loop

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -5,18 +5,27 @@ public class Game implements Runnable {
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
+	private boolean running = false;
 	private Thread gameThread;
 	
 	private synchronized void start(){
+		running = true;
 		gameThread  = new Thread(this, "Display");
 		gameThread.start();
 	}
 	
 	private synchronized void stop(){
+		running = false;
 		try {
 			gameThread.join();
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while(running) {
+			
 		}
 	}
 }
