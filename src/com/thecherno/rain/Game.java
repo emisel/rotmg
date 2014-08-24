02cddce4d7897f0755800cd7c58d102170c9b37d
emisel <eselmeryd@gmail.com>
2014-08-24 22:21:01 +0200

    Ep 4 - Window

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,12 +1,27 @@
 package com.thecherno.rain;
 
-public class Game implements Runnable {
+import java.awt.Canvas;
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	private static final long serialVersionUID = 1L;
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
 	
 	private boolean running = false;
+	private JFrame frame;
 	private Thread gameThread;
+	
+	public Game() {
+		Dimension size = new Dimension(width * scale, height * scale);
+		setPreferredSize(size);
+		
+		frame = new JFrame();
+	}
 	
 	private synchronized void start(){
 		running = true;
@@ -25,7 +40,18 @@ public class Game implements Runnable {
 	
 	public void run() {
 		while(running) {
-			
+			System.out.println("running...");
 		}
+	}
+	public static void main(String[] args) {
+		Game g = new Game();
+		g.frame.setResizable(false);
+		g.frame.setTitle("Rain");
+		g.frame.add(g);
+		g.frame.pack();
+		g.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		g.frame.setLocationRelativeTo(null);
+		g.frame.setVisible(true);
+		g.start();
 	}
 }
