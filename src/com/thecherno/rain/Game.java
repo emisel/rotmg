df42dcd0b37213035914fd27987c1d1534fa7a8e
emisel <eselmeryd@gmail.com>
2014-08-24 21:53:17 +0200

    Eppisode 2 - Threads

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -1,7 +1,22 @@
 package com.thecherno.rain;
 
-public class Game {
+public class Game implements Runnable {
 	public static int width = 300;
 	public static int height = width / 16 * 9;
 	public static int scale = 3;
+	
+	private Thread gameThread;
+	
+	private synchronized void start(){
+		gameThread  = new Thread(this, "Display");
+		gameThread.start();
+	}
+	
+	private synchronized void stop(){
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
 }
