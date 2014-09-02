dec33d95f4be4fe3a94ffc1fc33e2b586ad5ae91
emisel <eselmeryd@gmail.com>
2014-09-02 19:34:34 +0200

    Ep 39 - entity framework

---
+++ src/com/thecherno/rain/entity/Entity.java
@@ -0,0 +1,29 @@
+package com.thecherno.rain.entity;
+
+import java.util.Random;
+
+import com.thecherno.rain.level.Level;
+
+public class Entity {
+
+	private int x,y;
+	private boolean removed = false;
+	protected Level level;
+	protected final Random random = new Random();
+	
+	public void update(){
+		
+	}
+	
+	public void render (){
+		
+	}
+	
+	public void remove() {
+		//Remove from level
+		removed = false;
+	}
+	public boolean isRemoved(){
+		return removed;
+	} 
+}
