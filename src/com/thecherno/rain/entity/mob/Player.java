7d2cf034aebe3febb1334e6480e63e4d0fb3e886
emisel <eselmeryd@gmail.com>
2014-09-04 01:31:19 +0200

    fix

--- src/com/thecherno/rain/entity/mob/Player.java
+++ src/com/thecherno/rain/entity/mob/Player.java
@@ -31,7 +31,6 @@ public class Player extends Mob {
 	}
 	
 	public void render(Screen screen){
-	
 //		screen.renderPlayer(xx, yy, Sprite.player0);
 //		screen.renderPlayer(xx+16, yy, Sprite.player1);
 //		screen.renderPlayer(xx, yy+16, Sprite.player2);
@@ -42,6 +41,7 @@ public class Player extends Mob {
 		if (dir == 2) sprite = Sprite.player_down;
 		if (dir == 0) sprite = Sprite.player_up;
 		
+		screen.renderPlayer(x+16, y+16, sprite);
 	}
 
 }
