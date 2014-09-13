da5b7d65c85607de1993903850e4c9d42c34af39
emisel <eselmeryd@gmail.com>
2014-09-13 12:34:28 +0200

    Ep 56 mapping right

--- src/com/thecherno/rain/level/Level.java
+++ src/com/thecherno/rain/level/Level.java
@@ -6,9 +6,9 @@ import com.thecherno.rain.level.tile.Tile;
 
 public class Level {
 
-	protected Tile tiles [];
 	protected int width,height;
 	protected int [] tilesInt;
+	protected int [] tiles;
 
 	public Level (int width, int height){
 		this.width = width;
@@ -52,13 +52,7 @@ public class Level {
 
 		for (int y = y0; y < y1; y++){
 			for (int x= x0 ; x < x1; x++){
-				if ((x + y * 16) < 0 || (x + y * 16) >= 256) {
-					Tile.voidTile.render(x, y, screen) ;
-					continue;
-				}
-
-				
-					tiles[x + y * 16].render(x, y, screen);
+			getTile(x,y).render(x, y, screen);
 				
 			}
 		}
@@ -66,9 +60,9 @@ public class Level {
 
 	public Tile getTile(int x, int y){
 		if (x < 0 || y < 0 || y >= height || x >= width) return Tile.voidTile;
-		if (tilesInt[x + y * width] == 0) return Tile.grass;
-		if (tilesInt[x + y * width] == 1) return Tile.rock;
-		if (tilesInt[x + y * width] == 2) return Tile.flower;
+		if (tiles[x + y * width] == 0xFF00FF00) return Tile.grass;
+		if (tiles[x + y * width] == 0xFF7F7F00) return Tile.rock;
+		if (tiles[x + y * width] == 0xFFFFFF00) return Tile.flower;
 		return Tile.voidTile;
 	}
 
--- src/com/thecherno/rain/level/SpawnLevel.java
+++ src/com/thecherno/rain/level/SpawnLevel.java
@@ -10,7 +10,6 @@ import com.thecherno.rain.level.tile.Tile;
 
 public class SpawnLevel extends Level {
 
-	private int[] levelPixels;
 
 	public SpawnLevel(String path) {
 		super(path);
@@ -22,9 +21,8 @@ public class SpawnLevel extends Level {
 			img = ImageIO.read(SpawnLevel.class.getResource(path));
 			int w = img.getWidth();
 			int h = img.getHeight();
-			tiles = new Tile[w * h];
-			levelPixels = new int[w * h];
-			img.getRGB(0, 0, w, h, levelPixels, 0, w);
+			tiles = new int[w * h];
+			img.getRGB(0, 0, w, h, tiles, 0, w);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -32,20 +30,10 @@ public class SpawnLevel extends Level {
 		}
 	}
 	
-	//grass 0x00FF00
-	//rock 0x7F7F00
-	//flower 0xFFFF00
+	//grass 0xFF00FF00
+	//rock 0xFF7F7F00
+	//flower 0xFFFFFF00
 	protected void generateLevel() {
-		for (int i = 0; i < levelPixels.length;i++){
-			Color c0 = new Color(levelPixels[i]);
-			Color c = new Color(0xFF7F7F00);
-			System.out.println(c0.getBlue()+":"+c0.getGreen() + ":" +c0.getRed());
-			System.out.println(c.getBlue()+":"+c.getGreen() + ":" +c.getRed());
-			if (levelPixels[i] == 0xFF00FF00) tiles[i] = Tile.grass;
-			
-			if (levelPixels[i] == 0xFF7F7F00) tiles[i] = Tile.rock;
-			if (levelPixels[i] == 0xFFFFFF00) tiles[i] = Tile.flower;
-		}
-		
+
 	}
 }
