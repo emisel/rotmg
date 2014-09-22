2e5af8035c96ce91edeca8d6e5014146bb7a17fb
emisel <eselmeryd@gmail.com>
2014-09-22 21:14:05 +0200

    Ep 68 - more  mouse dir

--- src/com/thecherno/rain/Game.java
+++ src/com/thecherno/rain/Game.java
@@ -23,9 +23,9 @@ import com.thecherno.rain.level.TileCoordinate;
 public class Game extends Canvas implements Runnable {
 
 	private static final long serialVersionUID = 1L;
-	public static int width = 300;
-	public static int height = width / 16 * 9;
-	public static int scale = 3;
+	private static int width = 300;
+	private static int height = width / 16 * 9;
+	private static int scale = 3;
 	
 	private Screen screen;
 	private BufferedImage view = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
@@ -148,5 +148,11 @@ public class Game extends Canvas implements Runnable {
 		g.frame.setLocationRelativeTo(null);
 		g.frame.setVisible(true);
 		g.start();
+	}
+	public static int getWindowWidth(){
+		return width * scale;
+	}
+	public static int getWindowHeight() {
+		return height * scale;
 	}
 }
