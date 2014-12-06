3950bd2eedb4ee80ce6b10079d68f54c6e3d0af5
emisel <eselmeryd@gmail.com>
2014-12-06 11:19:14 +0100

    ep 81 - pixel perfect

--- src/com/thecherno/rain/entity/projectile/MonsterProjectile.java
+++ src/com/thecherno/rain/entity/projectile/MonsterProjectile.java
@@ -22,7 +22,7 @@ public class MonsterProjectile extends Projectile {
 	}
 	
 	public void update() {
-		if (level.tileCollision(x, y, nx, ny, 7)) {
+		if (level.tileCollision((int)(x + nx), (int)(y + ny), 7 ,4 ,4)) {
 			level.add(new ParticleSpawner((int)x,(int)y, 44,100,level));
 			remove();
 		}
--- src/com/thecherno/rain/level/Level.java
+++ src/com/thecherno/rain/level/Level.java
@@ -79,13 +79,13 @@ public class Level {
 		return projectiles;
 	}
 
-	public boolean tileCollision(double x, double y,double xa, double ya, int size) {
+	public boolean tileCollision(int x, int y, int size,int xOffset, int yOffset) {
 
 		boolean solid = false;
 
-		for (int c = 0; c<4; c++){
-			int xt = (((int)x+(int)xa) + c % 2 *  - 10 ) / 16;
-			int yt = (((int)y+(int)ya) + c / 2 * 2- 6) / 16;
+		for (int c = 0; c < 4; c++){
+			int xt = (x - c % 2 *  size + xOffset) >> 4;
+			int yt = (y - c / 2 * size + yOffset) >> 4;
 
 			if (getTile(xt, yt).solid()) solid = true;
 		}
